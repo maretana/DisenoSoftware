0079ca4cafcf4af173b86d53c0d870926b89a033
Mario Retana <maretana@ic-itcr.ac.cr>
2013-05-23 02:16:45 -0600

    Inicio de clase de persistencia para el modelo cliente.

---
+++ Sistema de Promociones/src/java/persistencia/ClientePersistencia.java
@@ -0,0 +1,26 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase de persistencia que se encarga de buscar y guardar los datos relacionados a los clientes en la BD.
+ * @author Mario Retana Rojas <201029799>
+ */
+public class ClientePersistencia implements JDBCProperties{
+    
+    private Connection _conexion;
+    
+    public ClientePersistencia(){
+        try {
+            this._conexion = DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            System.err.println("Imposible hacer conexion con la Base de Datos.");
+            System.err.println(ex.getMessage());
+            this._conexion = null;
+        }//fin catch
+    }//fin constructor
+}//fin cliente persistencia
