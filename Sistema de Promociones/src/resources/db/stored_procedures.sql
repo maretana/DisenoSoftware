52fdd5c4cc53d83a7247934fdd8968d7d572490f
Mario Retana <maretana@ic-itcr.ac.cr>
2013-05-24 17:01:03 -0600

    Procedure contar puntos del cliente por promocion por empresa.

--- Sistema de Promociones/src/resources/db/stored_procedures.sql
+++ Sistema de Promociones/src/resources/db/stored_procedures.sql
@@ -10,3 +10,20 @@ CREATE PROCEDURE buscarClientePorID(IN pIdentificacion VARCHAR(45))
 	END$$
 
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS contarPuntosClientePorPromocionPorEmpresa;
+
+DELIMITER $$
+/*Busca los puntos que tiene*/
+CREATE PROCEDURE contarPuntosClientePorPromocionPorEmpresa(IN pIdentificacion VARCHAR(45))
+    BEGIN
+        SELECT SUM(DCC.cantidad*PP.puntos) puntos, P.nombre promocion, E.nombre empresa FROM Clientes C
+        INNER JOIN HistorialComprasClientes HCC ON C.idCliente = HCC.idCliente
+        INNER JOIN DetallesCompraCliente DCC ON HCC.idHistorialComprasClientes = DCC.idHistorialComprasClientes
+        INNER JOIN Productos Pr ON DCC.idProducto = Pr.idProducto
+        INNER JOIN Productos_por_Promociones PP ON Pr.idProducto = PP.idProducto
+        INNER JOIN Promociones P ON PP.idPromocion = P.idPromocion
+        INNER JOIN Empresas E ON P.idEmpresa = E.idEmpresa
+        WHERE C.identificacion = pIdentificacion
+        GROUP BY P.idPromocion;
+    END$$
