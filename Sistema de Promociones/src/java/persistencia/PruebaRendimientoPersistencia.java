66dbb82707eaef5c714c39e8f16e7652ec6ed5d9
Mario Retana <maretana@ic-itcr.ac.cr>
2013-05-26 16:02:21 -0600

    Se agrega la clase que se comunica con la BD para las pruebas de rendimiento.

---
+++ Sistema de Promociones/src/java/persistencia/PruebaRendimientoPersistencia.java
@@ -0,0 +1,129 @@
+package persistencia;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+import modelo.Cliente;
+
+/**
+ * Esta clase es usada por la parte de la aplicación que hace pruebas de rendimiento. Se encarga
+ * de la conexión con la BD.
+ * @author Mario Retana Rojas <201029799>
+ */
+public class PruebaRendimientoPersistencia implements JDBCProperties{
+
+    private Connection _conexion;
+
+    public PruebaRendimientoPersistencia(){
+        try {
+            Class.forName(DRIVER);//Carga el driver del motor de BD en el servidor
+            this._conexion = DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
+            this._conexion.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            this._conexion.setAutoCommit(false);
+        } catch (SQLException ex) {
+            System.err.println("Imposible hacer conexion con la Base de Datos.");
+            System.err.println(ex.getMessage());
+            this._conexion = null;
+        } catch (ClassNotFoundException ex) {
+            System.err.println(ex.getMessage());
+            this._conexion = null;
+        }//fin catch
+    }//fin constructor
+
+    /**
+     * Registra un cliente en la base de datos usando el procedure especial de pruebas de rendimiento.
+     * @param pCliente Cliente que va a ser registrado en la BD.
+     * @return Retorna <code>true</code> si pudo agregar los datos con éxito y <code>false</code> de lo contrario.
+     */
+    public boolean registrarCliente(Cliente pCliente){
+        String sql = "{call registrarClientePruebaRendimiento(?,?,?,?)} ";
+        try{
+            CallableStatement call = this._conexion.prepareCall(sql);
+            call.setString(1, pCliente.getNombre());
+            call.setString(2, pCliente.getApellido1());
+            call.setString(3, pCliente.getApellido2());
+            call.setString(4, pCliente.getIdentificacion());
+            call.execute();
+            this._conexion.commit();
+        }//fin try
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            try{
+                this._conexion.rollback();
+            }//fin try
+            catch (SQLException sqlex){
+                System.err.println("Error al intentar hacer rollback!");
+                System.err.println(sqlex.getMessage());
+            }//fin catch
+            return false;
+        }//fin catch
+        return true;
+    }//fin resgistrar cliente
+    
+    /**
+     * Registra una compra de un cliente en particular para realizar pruebas de rendimiento.
+     * @param pCodigo Semilla que usará la base de datos para generar un código de factura.
+     * @param pIdentificacion Identificación del cliente al que se le registra la compra.
+     * @param pDias La compra queda regitrada que se hizo hace pDias a partir de la fecha actual de la BD.
+     * @return Retorna el código de la factura autogenerado por la BD. Retorna <code>null</code> si ocurre
+     *         algún error durante la conexión a la BD.
+     */
+    public String registrarNuevaCompra(String pCodigo, String pIdentificacion, int pDias){
+        String sql = "{call registrarNuevaCompraPruebaRendimiento(?,?,?)}";
+        String codigo_bd = pCodigo;
+        try{
+            CallableStatement call = this._conexion.prepareCall(sql);
+            call.registerOutParameter(1, Types.CHAR);
+            call.setString(2, pIdentificacion);
+            call.setInt(3, pDias);
+            call.execute();
+            this._conexion.commit();
+        }//fin try
+        catch (Exception e){
+            System.err.println(e.getMessage());
+            try{
+                this._conexion.rollback();
+            }//fin try
+            catch(SQLException sqlex){
+                System.err.println("Error al hacer rollback!");
+                System.err.println(sqlex.getMessage());
+            }//fin catch
+            return null;
+        }//fin exception e
+        return codigo_bd;
+    }//fin registrar nueva compra
+    
+    /**
+     * Ingresa un detalle a la factura con el codigo dado.
+     * @param pCodigo   Codigo de la factura a la que se agrega el detalle.
+     * @param pEmpresa  Nombre de la empresa en donde se hizo la compra.
+     * @param pProducto Nombre del producto que se compró.
+     * @param pCantidad Cantidad de productos del mismo nombre que se compraron.
+     * @return Retorna <code>true</code> si logra insertar el detalle a la BD o <code>false</code> de lo contrario.
+     */
+    public boolean ingresarDetalleCompra(String pCodigo, String pEmpresa, String pProducto, int pCantidad){
+        String sql = "{call insertarDetalleCompraPruebaRendimiento(?,?,?,?)}";
+        try{
+            CallableStatement call = this._conexion.prepareCall(sql);
+            call.setString(1, pCodigo);
+            call.setString(2, pProducto);
+            call.setInt(3, pCantidad);
+            call.setString(4, pEmpresa);
+        }//fin try
+        catch (Exception e){
+            System.err.println(e.getMessage());
+            try{
+                this._conexion.rollback();
+            }//fin try
+            catch (SQLException sqlex){
+                System.err.println("Error al hacer rollback!");
+                System.err.println(sqlex.getMessage());
+            }//fin catch
+            return false;
+        }//fin catch
+        return true;
+    }//fin ingresar detalle de compra
+
+}//fin clase pruebas rendimiento persistencia
