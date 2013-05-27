1d327b291bebb929d71c5d3a1a9c2578406fa770
Mario Retana <maretana@ic-itcr.ac.cr>
2013-05-27 02:58:53 -0600

    Se agrega la clase reporte.

---
+++ Sistema de Promociones/src/java/modelo/Reporte.java
@@ -0,0 +1,81 @@
+package modelo;
+
+/**
+ * Esta clase contiene la informacion que sera puesta en el reporte.
+ * @author Mario Retana Rojas 201029799
+ */
+public class Reporte {
+    
+    private int _usuariosRegistrados;
+    private int _empresasRegistradas;
+    private int _productosComprados;
+    private int _comprasRegistradas;
+    private int _promocionesRegistradas;
+    private int _productosRegistrados;
+    private int _premiosRegistrados;
+    
+    public Reporte(){
+    }//fin constructor
+
+    //<editor-fold defaultstate="collapsed" desc="Getters y Setters">
+    public int getPremiosRegistrados() {
+        return _premiosRegistrados;
+    }
+    
+    public void setPremiosRegistrados(int premiosRegistrados) {
+        this._premiosRegistrados = premiosRegistrados;
+    }
+    
+    
+    public int getProductosRegistrados() {
+        return _productosRegistrados;
+    }
+    
+    public void setProductosRegistrados(int productosRegistrados) {
+        this._productosRegistrados = productosRegistrados;
+    }
+    
+    public int getPromocionesRegistradas() {
+        return _promocionesRegistradas;
+    }
+    
+    public void setPromocionesRegistradas(int promocionesRegistradas) {
+        this._promocionesRegistradas = promocionesRegistradas;
+    }
+    
+    public int getComprasRegistradas() {
+        return _comprasRegistradas;
+    }
+    
+    public void setComprasRegistradas(int comprasRegistradas) {
+        this._comprasRegistradas = comprasRegistradas;
+    }
+    
+    public int getProductosComprados() {
+        return _productosComprados;
+    }
+    
+    public void setProductosComprados(int productosComprados) {
+        this._productosComprados = productosComprados;
+    }
+    
+    
+    public int getEmpresasRegistradas() {
+        return _empresasRegistradas;
+    }
+    
+    public void setEmpresasRegistradas(int empresasRegistradas) {
+        this._empresasRegistradas = empresasRegistradas;
+    }
+    
+    
+    public int getUsuariosRegistrados() {
+        return _usuariosRegistrados;
+    }
+    
+    public void setUsuariosRegistrados(int usuariosRegistrados) {
+        this._usuariosRegistrados = usuariosRegistrados;
+    }
+    //</editor-fold>
+
+}//fin clase reporte
