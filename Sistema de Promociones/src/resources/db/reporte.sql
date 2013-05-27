fd9113762c25157f10915a5587faaa0c7ed740da
Mario Retana <maretana@ic-itcr.ac.cr>
2013-05-27 12:29:01 -0600

    SP para el reporte

---
+++ Sistema de Promociones/src/resources/db/reporte.sql
@@ -0,0 +1,28 @@
+/* Estos stored procedures se usan para recolectar datos interesantes para el
+ * reporte al usuario.
+ */
+
+-----------------------------contarDatosReporte---------------------------------------------
+DROP PROCEDURE IF EXISTS contarDatosReporte;
+
+DELIMITER $$
+
+CREATE PROCEDURE contarDatosReporte()
+    BEGIN
+        (SELECT 'empresas' nombre, COUNT(*) total FROM Empresas)
+        UNION
+        (SELECT 'promos', COUNT(*) FROM Promociones)
+        UNION
+        (SELECT 'premios', COUNT(*) FROM Premios)
+        UNION
+        (SELECT 'productos', COUNT(*) FROM Productos)
+        UNION
+        (SELECT 'comprados', SUM(cantidad) FROM DetallesCompraCliente)
+        UNION
+        (SELECT 'compras', COUNT(*) FROM HistorialComprasCliente)
+        UNION 
+        (SELECT 'clientes', COUNT(*) FROM Clientes);
+    END$$
+
+DELIMITER ;
+-----------------------------contarDatosReporte---------------------------------------------
